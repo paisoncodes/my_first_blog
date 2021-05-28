f4a5e90a855387f986684c4ba12c4c62c387696e
Olatunji <iamkomolafe.o.s@gmail.com>
2021-05-28 23:51:12 +0100

    fixed the registration form error

--- account/views.py
+++ account/views.py
@@ -11,7 +11,7 @@ def registration_view(request):
         if form.is_valid():
             form.save()
             email = form.cleaned_data.get('email')
-            raw_password = form.cleaned_data.get('password')
+            raw_password = form.cleaned_data.get('password1')
             account = authenticate(email=email, password=raw_password)
             login(request, account)
             return redirect('home')
